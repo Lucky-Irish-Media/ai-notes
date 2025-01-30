3a314d31fabeecbb645d344a894e4be6583001b6
Ian Link <ian.link@luckyirishmedia.com>
2025-01-30 11:56:40 -0500

    Tweak transcription code

--- whisper-notes.py
+++ whisper-notes.py
@@ -1,11 +1,14 @@
 import argparse
+import os
+import pydub
+import whisper
+import time
+import numpy as np
 
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import Ollama
-import whisper
-import time
 
 stt = whisper.load_model("base.en")
 
@@ -16,10 +19,35 @@ You are an AI specializing in summarizing transcribed voice notes. Below is a tr
 PROMPT = PromptTemplate(input_variables=["history","input"], template=template)
 chain = ConversationChain(
     prompt=PROMPT,
-    verbose=True,
+    verbose=False,
     memory=ConversationBufferMemory(ai_prefix="Assistant:"),
-    llm=Ollama(model="llama3.3"),
+    llm=Ollama(model="llama3.2"),
 )
+
+def chunk_audio(audio_file):
+    """
+    Breaks an audio file into chunks of a specified duration.
+
+    Parameters:
+        audio_file (str): Path to the input audio file.
+
+    Returns:
+        Numpy array: Array of sound chunks.
+    """
+
+    # Load the audio file
+    sound = pydub.AudioSegment.from_file(audio_file)
+    sounds = pydub_to_np(sound)
+
+    return sounds
+
+def pydub_to_np(audio: pydub.AudioSegment):
+    """
+    Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
+    where each value is in range [-1.0, 1.0]. 
+    Returns tuple (audio_np_array, sample_rate).
+    """
+    return np.array(audio.get_array_of_samples(), dtype=np.int16).astype(np.float32) / 32768.0
 
 def transcribe(audio) -> str:
     """
@@ -63,21 +91,24 @@ def main():
         metavar='PATH',
         help='Absolute paths to the audio files to be transcribed')
     args = parser.parse_args()
-    
-    for audio_path in args.audio_paths:
-            transcript = transcribe(audio_path)
-            if (args.verbose):
-                print(transcript)
-                
-            summary = get_llm_response(transcript)
-            if (args.verbose):
-                print(summary)
 
-            notes = summary+"\n\n---\n\nFull Transcript:\n"+transcript
+    for audio in args.audio_paths:
+        # chunks = chunk_audio(audio)
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    with open("meeting-summary-"+timestr+".md", "w") as f:
-        f.write(notes)
+        transcript = transcribe(audio)
+
+        if (args.verbose):
+            print(transcript)
+
+        summary = get_llm_response(transcript)
+        if (args.verbose):
+            print(summary)
+
+        notes = summary+"\n\n---\n\nFull Transcript:\n"+transcript
+
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        with open("meeting-summary-"+timestr+".md", "w") as f:
+            f.write(notes)
 
 if __name__ == '__main__':
     main()
